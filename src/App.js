b8800f8d77434d4d97d848be64a565f417410fcf
Emily <e3tao@uwaterloo.ca>
2020-03-16 23:11:30 -0400

    Added about me section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomePage from './components/HomePage'
+import AboutMe from './components/AboutMe'
 import './css/main.scss';
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <HomePage/>
+      <AboutMe/> 
     </div> 
   );
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import linkedin from "../assets/linkedin.svg"
-import github from "../assets/github.svg"
+import linkedin from "../assets/linkedin_white.svg"
+import github from "../assets/github_white.svg"
 
 class HomePage extends React.Component {
     render () {
         return (
-            <div> 
-                <div className="h1 title"> emily tao </div>
-                <div className="h2 description"> developer, engineer, architect </div>
-                <a href="https://www.linkedin.com/in/emilytao2000/" target="_blank" rel="noopener noreferrer"> <img className="linkedin" src={linkedin} alt="" /></a>
-                <a href="https://github.com/emilytao" target="_blank" rel="noopener noreferrer"> <img src={github} alt="" /></a>
+            <div className="home"> 
+                <div className="h1 home__title"> emily tao </div>
+                <div className="h2 home__description"> developer, engineer, designer </div>
+                <a href="https://www.linkedin.com/in/emilytao2000/" target="_blank" rel="noopener noreferrer"> <img className="icon linkedin" src={linkedin} alt="" /></a>
+                <a href="https://github.com/emilytao" target="_blank" rel="noopener noreferrer"> <img  className="icon" src={github} alt="" /></a>
             </div>
         );
     }
