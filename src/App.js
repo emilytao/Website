17146a0a9e8272710c8bb2d3a11d7fb9fe870496
Emily <e3tao@uwaterloo.ca>
2020-03-17 17:42:01 -0400

    Added scrolling function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useRef } from "react";
+
 import HomePage from './components/HomePage'
 import AboutMe from './components/AboutMe'
 import './css/main.scss';
 
 function App() {
+  let myRef = useRef(null);
+  const onButtonClick = () => {
+    window.scrollTo({ behavior: "smooth", top: myRef.current.offsetTop });
+  };
+
   return (
     <div className="container">
       <HomePage/>
+      <button onClick={onButtonClick}>Focus the input</button>
+      
       <AboutMe/> 
+      <div ref={myRef}>My Component</div>
     </div> 
   );
 }
